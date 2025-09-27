e7986b83cb21b6d721efe27db274f35496e81e7e
Jhonny Rodriguez <Jhonnyrodri156@gmail.com>
2025-09-26 21:13:09 -0500

    Añadimos Rescontroler para funcionalidades en producto

--- src/main/java/com/magicGroup/backend/repository/ProductoRepository.java
+++ src/main/java/com/magicGroup/backend/repository/ProductoRepository.java
@@ -6,11 +6,18 @@
 package com.magicGroup.backend.repository;
 
 import com.magicGroup.backend.model.Producto;
+import com.magicGroup.backend.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  *
  * @author Jhonn
  */
-
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    List<Producto> findByCategoria(Categoria categoria);
+    List<Producto> findByNombreContainingIgnoreCase(String nombre);
 }
